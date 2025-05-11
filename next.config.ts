30cc68da6cebe00d6cdeb99347b52164e9916415
mojizaza12 <mojizaza12@bannawat.store>
2025-05-11 21:30:46 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  module.exports = {
-  webpack: (config, { isServer }) => {
-    // Prevent loading the problematic SWC modules
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@next/swc-linux-x64-gnu': false,
-      '@next/swc-linux-x64-musl': false
-    }
-    return config
-  }
-}
+  
 };
 
 export default nextConfig;
